f0c7b6d3fb194f0c33e08c48ae9470d55b8113d3
Ganjar <ganjarggt@gmail.com>
2021-12-15 15:45:26 +0700

    implement custom validation

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,31 @@
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationError, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationException } from './filters/validation.exception';
+import { ValidationFilter } from './filters/validation.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
-  app.useGlobalPipes(new ValidationPipe());
+  // app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new ValidationFilter());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      skipMissingProperties: false,
+      exceptionFactory: (errors: ValidationError[]) => {
+        const messages = errors.map((error) => {
+          return {
+            property: error.property,
+            message: Object.keys(error.constraints).map(
+              (key) => error.constraints[key],
+            ),
+          };
+        });
+        return new ValidationException(messages);
+      },
+    }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
