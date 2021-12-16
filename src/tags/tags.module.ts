adef58982e4e8ec209524901a58d29d6bcfbf15f
Ganjar <ganjarggt@gmail.com>
2021-12-16 16:15:03 +0700

    implement auth admin guard

--- src/tags/tags.module.ts
+++ src/tags/tags.module.ts
@@ -3,9 +3,11 @@ import { TagsService } from './tags.service';
 import { TagsController } from './tags.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Tag } from './entities/tag.entity';
+import { AuthModule } from 'src/auth/auth.module';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Tag])],
+  imports: [TypeOrmModule.forFeature([Tag]), AuthModule, UsersModule],
   controllers: [TagsController],
   providers: [TagsService],
   exports: [TypeOrmModule, TagsService],
