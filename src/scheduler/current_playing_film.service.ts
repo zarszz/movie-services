d6786b8f39804ab1e36c69e836dacc4ae10bb8f3
Ganjar <ganjarggt@gmail.com>
2021-12-17 15:52:30 +0700

    add movie scheduling

--- src/scheduler/current_playing_film.service.ts
+++ src/scheduler/current_playing_film.service.ts
@@ -3,17 +3,18 @@ import { Cron, CronExpression } from '@nestjs/schedule';
 import axios from 'axios';
 import { ConfigService } from 'src/config/config.service';
 import { Movie } from 'src/movies/entities/movie.entity';
+import { MoviesService } from 'src/movies/movies.service';
 
 @Injectable()
 export class CurrentPlayingFilmService {
-  // @Cron(CronExpression.EVERY_10_SECONDS)
-  // async handleCron() {
-  //   const key = new ConfigService().get('THEMOVIEDB_KEY');
-  //   const response = await axios.get(
-  //     `https://api.themoviedb.org/3/movie/now_playing?api_key=${key}&language=en-US&page=1`,
-  //   );
-  //   const movies: Movie[] = response.data.results;
-  //   console.log(movies);
-  //   // TODO : insert film data to db
-  // }
+  constructor(private readonly movieService: MoviesService) {}
+  @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
+  async handleCron() {
+    const key = new ConfigService().get('THEMOVIEDB_KEY');
+    const response = await axios.get(
+      `https://api.themoviedb.org/3/movie/now_playing?api_key=${key}&language=en-US&page=1`,
+    );
+    const movies: Movie[] = response.data.results;
+    this.movieService.bulkInsert(movies);
+  }
 }
