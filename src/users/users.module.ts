53e75aad938f3eb837a1948b979c7a6d6044921c
Ganjar <ganjarggt@gmail.com>
2021-12-23 14:14:29 +0700

    add guard to users module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,12 +1,13 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entity/user.entity';
 import { UserSeederService } from './user.seeder.service';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), forwardRef(() => AuthModule)],
   controllers: [UsersController],
   providers: [UsersService, UserSeederService],
   exports: [TypeOrmModule, UsersService, UserSeederService],
