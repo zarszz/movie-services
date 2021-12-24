1c8a6a53180422bdbebe82c4b247835452a31ce0
Ganjar <ganjarggt@gmail.com>
2021-12-24 13:51:07 +0700

    remove 10 seconds cronjob

--- src/scheduler/scheduler.module.ts
+++ src/scheduler/scheduler.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MoviesModule } from 'src/movies/movies.module';
 import { CurrentPlayingFilmService } from './current-playing-film.service';
-import { HealthService } from './health.service';
 
 @Module({
   imports: [MoviesModule],
-  providers: [CurrentPlayingFilmService, HealthService],
-  exports: [CurrentPlayingFilmService, HealthService],
+  providers: [CurrentPlayingFilmService],
+  exports: [CurrentPlayingFilmService],
 })
 export class SchedulerModule {}
